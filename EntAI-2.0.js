2a82c52360da539eec7c8af7dee5b983cdcae476
muno9748 <58895791+muno9748@users.noreply.github.com>
2020-04-23 20:48:32 +0900

    2.1

--- EntAI-2.0.js
+++ EntAI-2.0.js
@@ -16,7 +16,7 @@ Entry.events_.showMachineLearningScreen[1] = () => {$('.description__d9e8f').htm
     `머신러닝에 필요한 도구들을 로드중입니다.<br />몇 초만 기다려 주세요.<br />${window.EntAI_loadedPackageCount} / ${window.EntAI_totalPackageCount}`
 )}
 (async () => {
-    const MODELDATA = (await $.get('https://cdn.jsdelivr.net/gh/muno9748/EntAI@latest/models.json'));
+    const MODELDATA = JSON.parse(await $.get('https://raw.githubusercontent.com/muno9748/EntAI/master/models.json'));
     if(Entry.variableContainer.getListByName('EntAI-모델')) {
         const Models = (() => {return returnvalue = {}, Object.keys(MODELDATA).map((el,i) => {return {
             [MODELDATA[el].name]:MODELDATA[el].modelname
@@ -52,6 +52,11 @@ Entry.events_.showMachineLearningScreen[1] = () => {$('.description__d9e8f').htm
         }}).forEach((el,i) => {
             returnvalue[Object.keys(el)[0]] = el[Object.keys(el)[0]]
         }),returnvalue})();
+        const modelScript = (() => {return returnvalue = {}, Object.keys(MODELDATA).map((el,i) => {return {
+            [MODELDATA[el].modelname]:MODELDATA[el].script
+        }}).forEach((el,i) => {
+            returnvalue[Object.keys(el)[0]] = el[Object.keys(el)[0]]
+        }),returnvalue})();
         window.EntAI_loadedPackageCount = 0;
         window.EntAI_totalPackageCount = usingModels.length;
         $.getScript('https://cdn.jsdelivr.net/npm/@tensorflow/tfjs').then(() => {
@@ -59,8 +64,8 @@ Entry.events_.showMachineLearningScreen[1] = () => {$('.description__d9e8f').htm
                 Entry.dispatchEvent('showMachineLearningScreen');
                 try {
                     console.log(modelScriptUrls[Models[el]]);
-                    if(modelScriptUrls[Models[el]].length == 1) {
-                        $.getScript(modelScriptUrls[Models[el]][0], () => {
+                    if(!(modelScript[Models[el]])) {
+                        $.getScript(modelScriptUrls[Models[el]], () => {
                             if(modelParams[Models[el]] != null) {
                                 window[Models[el]].load(modelParams[Models[el]]).then(model => {
                                     window[modelDatas[Models[el]]] = model;
@@ -83,32 +88,8 @@ Entry.events_.showMachineLearningScreen[1] = () => {$('.description__d9e8f').htm
                                 });
                             }
                         });
-                    } else if (modelScriptUrls[Models[el]].length == 2) {
-                        $.getScript(modelScriptUrls[Models[el]][0], () => {
-                            $.getScript(modelScriptUrls[Models[el]][1], () => {
-                                if(modelParams[Models[el]] != null) {
-                                    window[Models[el]].load(modelParams[Models[el]]).then(model => {
-                                        window[modelDatas[Models[el]]] = model;
-                                        loadModel(Models[el]);
-                                        window.EntAI_loadedPackageCount++;
-                                        Entry.events_.showMachineLearningScreen[1]();
-                                        if(window.EntAI_loadedPackageCount == window.EntAI_totalPackageCount) {
-                                            Entry.dispatchEvent('hideLoadingScreen')
-                                        }
-                                    });
-                                } else {
-                                    window[Models[el]].load().then(model => {
-                                        window[modelDatas[Models[el]]] = model;
-                                        loadModel(Models[el]);
-                                        window.EntAI_loadedPackageCount++;
-                                        Entry.events_.showMachineLearningScreen[1]();
-                                        if(window.EntAI_loadedPackageCount == window.EntAI_totalPackageCount) {
-                                            Entry.dispatchEvent('hideLoadingScreen')
-                                        }
-                                    });
-                                }
-                            });
-                        });
+                    } else {
+                        eval(modelScript[Models[el]])();
                     }
                 } catch (e) {
                     Entry.toast.alert('EntAI 오류','알수없는 오류가 발생했습니다. (' + e.toString().replace(/\n/gi, ' ') + ')');
